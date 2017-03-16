3d2020455aa2e1b5f6ded516ec921954726df8d4
allthatilk <imogenkutz@hotmail.com>
2017-03-16 01:11:18 +0000

    WIP not sure how to test errors

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,11 +3,42 @@ describe("Thermostat", function() {
   beforeEach(function() {
     thermo = new Thermostat();
   });
-  describe("base temperature", function() {
-    it("is set at 20 degrees", function() {
+
+  describe("temperature", function() {
+    it("default is set at 20 degrees", function() {
       expect(thermo.temperature).toEqual(20);
+    });
+    it("goes up", function() {
+      thermo.tempIncrease(5);
+      expect(thermo.temperature).toEqual(25);
+    });
+    it("goes down", function() {
+      thermo.tempDecrease(10);
+      expect(thermo.temperature).toEqual(10);
+    });
+    it("doesn't go below 10 degrees", function() {
+      thermo.tempDecrease(10);
+      // var message = "Cannot go below 10 degrees"
+      expect(thermo.tempDecrease(1)).toEqual("Cannot go below 10 degrees")
     });
   });
 
-  
+  describe("power-saving mode", function() {
+    it("can turn power-saving mode off", function() {
+      console.log(thermo);
+      thermo.powerSavingOff();
+      expect(thermo.powerSaving).toEqual(false);
+    });
+    it("can turn power-saving mode on", function() {
+      thermo.powerSavingOff();
+      console.log(thermo);
+      thermo.powerSavingOn();
+      expect(thermo.powerSaving).toEqual(true);
+    });
+    it("changes the max temperature while power-saving is on", function() {
+      thermo.tempIncrease(5);
+      // var error = "Cannot go above 25 degrees when in power-saving mode"
+      expect(thermo.tempIncrease(1)).toEqual("Cannot go above 25 degrees when in power-saving mode")
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,29 @@
 function Thermostat() {
-  this.temperature = 20
+  this.temperature = 20;
+  this.powerSaving = true;
+};
+
+Thermostat.prototype.tempIncrease = function(amount) {
+  this.temperature += amount;
+};
+
+Thermostat.prototype.tempDecrease = function(amount) {
+  if (this.checkMin() === true)
+  console.log("Cannot go below 10 degrees");
+  else {
+    this.temperature -= amount
+  };
+};
+
+Thermostat.prototype.checkMin = function() {
+  this.temperature <= 10
+  true;
+};
+
+Thermostat.prototype.powerSavingOff = function() {
+  this.powerSaving = false;
+};
+
+Thermostat.prototype.powerSavingOn = function() {
+  this.powerSaving = true;
 };
